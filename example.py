47bb661dc2126152addf97b8b58d3a39602704e6
Matt Clarke <matt.clarke@esss.se>
2020-11-30 10:24:40 +0100

    Comments in example.py

--- example.py
+++ example.py
@@ -1,9 +1,7 @@
-from html.parser import HTMLParser
-import os
-
-
 def csv_addition(buf):
-    # Convert a csv string into ints and then add them
+    """
+    Convert a csv string into ints and then add them.
+    """
     chars = buf.split(",")
     result = 0
     for c in chars:
@@ -12,6 +10,5 @@ def csv_addition(buf):
 
 
 if __name__ == '__main__':
-    with open("initial-inputs/input1.txt") as f:
-        i = f.read()
-    print(csv_addition(i))
+    # Not relevant to fuzzing
+    print(csv_addition("1,2,3"))
